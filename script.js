a2a4e0b1cf854b1c980b4c857948b1b3ebd2888a
Tech Advanced OCI <techadvancedoci@gmail.com>
2026-04-02 15:39:33 -0300

    Fix Add function showQuestion() in function nextQuestion() line1802

--- script.js
+++ script.js
@@ -1799,7 +1799,7 @@ function nextQuestion() {
 
     if (currentQuestionIndex < questions.length - 1) {
     currentQuestionIndex++;
-    loadQuestion();
+    showQuestion();
   } else {
 
     // ===== TRACKING GOOGLE ANALYTICS =====
